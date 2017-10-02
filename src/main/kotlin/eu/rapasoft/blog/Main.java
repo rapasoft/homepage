84b672de83503f41c894c0d79d51f74f2e360f20
Pavol Rajzak <pavol.rajzak@itera.no>
2017-10-02 14:59:07 +0200

    Commit new GH-pages version

--- src/main/kotlin/eu/rapasoft/blog/Main.java
+++
@@ -1,9 +0,0 @@
-package eu.rapasoft.blog;
-
-import eu.rapasoft.blog.service.BlogPostGenerator;
-
-public class Main {
-    public static void main(String[] args) {
-        new BlogPostGenerator().generateBlogPosts();
-    }
-}
